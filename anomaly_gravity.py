67103a5eafc8bf57da6ea2fe9599bd53809a6ab8
matheus <geof.matheus@gmail.com>
2022-05-17 14:44:29 -0300

    added gravity anomaly

--- anomaly_gravity.py
+++ anomaly_gravity.py
@@ -5,8 +5,8 @@ from scipy import integrate
 def gz(x,y,z,dens, xc, yc, zc, a, b, c):
     G = 6.67e-11
     xi = xc - a/2; xf = xc + a/2
-    yi = yc - a/2; yf = yc + a/2
-    zi = zc - a/2; zf = zc + a/2
+    yi = yc - b/2; yf = yc + b/2
+    zi = zc - c/2; zf = zc + c/2
     
     def kernel(xs, ys, zs):
         dx = x - xs; dy = y - ys; dz = z - zs
@@ -16,7 +16,7 @@ def gz(x,y,z,dens, xc, yc, zc, a, b, c):
     i = integrate.tplquad(kernel, zi, zf, yi, yf, xi, xf)
     return i[0]
 
-n = 50
+n = 20
 i = 0
 f = 100
 
@@ -28,19 +28,19 @@ X, Y = np.meshgrid(x,y)
 g = np.zeros((n,n), dtype=np.float64)
 for i in range(n):
     for j in range(n):
-        g[i][j] = gz(X[i][j], Y[i][j], 0, 200, 50, 50, 10,10,10, 5)
+        g[i][j] = gz(X[i][j], Y[i][j], -40, 200, 50, 50, 10,10,10, 5)
 
 plt.rcParams["figure.figsize"] = (8, 6)
 plt.contourf(X, Y, g, cmap = 'jet')
 plt.colorbar()
+plt.show()
 
 ax= plt.axes(projection='3d')
 ax.plot_surface(X, Y, g, cmap='jet')
 
 ax.set_xlabel('Eixo X')
 ax.set_ylabel('Eixo Y')
-ax.set_zlabel('mGal')
+ax.set_zlabel('Gravidade')
 
-ax.view_init(4,222)
 plt.rcParams["figure.figsize"] = (8, 6)
 plt.show()
